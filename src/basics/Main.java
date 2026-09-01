f0bd5e74d0f5d64a1fbd354d1040060d9cf27ed7
Naman-parashar-coder <namanparashar248@gmail.com>
2026-09-01 07:31:50 +0530

    Day 1-Array Traversal

---
+++ src/basics/Main.java
@@ -0,0 +1,7 @@
+package basics;
+
+public class Main {
+    static void main() {
+        System.out.println("Java Deep Learning - Day 1");
+    }
+}
---
+++ src/basics/arrays/ArrayTraversal.java
@@ -0,0 +1,18 @@
+package basics.arrays;
+
+public class ArrayTraversal {
+public static void main(String[] args){
+
+    int num[] = new int[4];
+    num[0]=1;
+    num[1]=2;
+    num[2]=4;
+    num[3]=9;
+
+    for(int i=0;i<num.length;i++){
+        System.out.println(num[i]);
+    }
+
+}
+}
+
